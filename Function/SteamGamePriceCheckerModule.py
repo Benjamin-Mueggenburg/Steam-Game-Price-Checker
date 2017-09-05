04c7dd3442b5a19af35a2d16077ad3c215a3e3c5
tintinmovie <31559358+tintinmovie@users.noreply.github.com>
2017-09-06 11:50:19 +1200

    Update SteamGamePriceCheckerModule.py
    Finally finished the complete module file for python

--- Function/SteamGamePriceCheckerModule.py
+++ Function/SteamGamePriceCheckerModule.py
@@ -1,21 +1,9 @@
 #Steam Game Price checker
 #By Benjamin
-#22/06/17
+#6/09/17
 
 #Import request, a library which allows python to access WebAPI's in the form of JSON
 import requests
-
-#Import os, allows for program to pause at end
-import os
-
-
-#System automatically ajusts currency to location. If you want to manually type it in (if you're using TOR or a VPN) then set
-#AutomaticCurreny to 'false'
-automaticCurrency = True
-#Change for currency (The New Zealand dollar is nzd, and United States dollar is us). 
-#This does not matter if 'automaticCurrency' is set to true
-desiredcurrency = "nzd"
-
 
 class SteamGameChecker:
 	def __init__(self,desiredcurrency,automaticCurrency=True):
@@ -49,67 +37,7 @@ class SteamGameChecker:
 		#currecy is the variable storing the currency of the prices of the game e.g NZD
 		currency = j["{}".format(appid)]["data"]["price_overview"]["currency"]
 		#returns all the variables in the form of a list
-		return [Gamename, intial_price, final_price, discountpercent, currency]
+		return {"gamename":Gamename, "intialprice":intial_price, "finalprice":final_price, "discountpercent":discountpercent, "currency":currency}
 
-#The PrintInfo function, once inputed with the game's url, appid and number(this was the variable at the top that equals 1) (Next line)
-#It will print out all the details about the game in a more unifrom way
-def PrintInfo(url,appid,number):
-	#Calls the above fuction call GetInformation (meaning that the variable, 'a' is now a list)
-	a = GetInformation(url, appid)
-	#Prints at the start "Game" and then the game number in the list which is inputed through the variable 'number'
-	print("Game {}".format(number))
-	#Prints the name of the game, which is stored in 'a' at number 0 in the list
-	print("Name: {}".format(a[0]))
-	#Prints the intial price and the currency of the game, which is stored in 'a' at number 1 and 4 in the list. It also adds in the $ symbol
-	print("Intial Price: ${} {}".format(a[1],a[4]))
-	#Prints the final price and the currency of the game, which is stored in 'a' at number 2 and 4 in the list. It also adds in the $ symbol
-	print("Final Price: ${} {}".format(a[2],a[4]))
-	#Prints the discount perecnt of the game, which is stored in 'a' at number 3 in the list. It also adds in the % symbol for the percent.
-	print("Discount Percent: {}%".format(a[3]))
 
-	#a[3] is the discount perecent, so this checks if the discount percent equals zero, meaning there is no deal or special
-	if a[3] == 0:
-		#Prints a seperation
-		print("*******************")
-		#If the statement is true, then print out "NO DISCOUNT AT THE MOMENT"
-		print("NO DISCOUNT AT CURRENT MOMENT")
 
-	#If the 'if' statement is false, meaning there is a deal, then run the following lines of code
-	else:
-		#Prints the price reduction, and since there is no variable, it takes the intial price(a[1]) and subtracts the final price(a[2]). It also adds in the currency(a[4])
-		print("Price Reduction: ${} {}".format(a[1] - a[2], a[4]))
-		#prints out a line of asterisks seperating the line after from all the numbers printed above
-		print("*******************")
-		#prints out the line "CURRENT DISCOUNT" indicating that there is some sort of deal or special on
-		print("CURRENT DISCOUNT")
-	#(No matter if the 'if' statement runs or the 'else' this following line will run) this seperates the chosen game from the next
-	print("==================================")
-
-#IF YOU WANT TO CHANGE THE GAMES THAT IT DISPLAYS YOU MUST CHANGE THE FOLLOWING STRINGS INSIDE OF THE LIST BELOW
-#First find the url of the game you want e.g Kerbal Space program - "http://store.steampowered.com/app/220200/Kerbal_Space_Program/".
-#Between /app/ and the name of the game there is a number e.g in kerbal space it's "220200"
-#With this number, add a ',' to the end of the last number, then paste your number there. Make sure to add "" marks around the number or it WILL NOT WORK 
-Filename = "AppID"
-with open('{}.txt'.format(Filename)) as f:
-		file = f.read().splitlines()
-
-appId = file[1:]
-
-#This loop runs for how many strings are in the list called appId
-for ID in appId:
-	#Checks to see if automatic currency is true or false
-	#If it is then the desired currency does not matter
-	if automaticCurrency = True:
-		#Creates a temporary url for the game, by inserting the appid into the url below
-		tempUrl = "http://store.steampowered.com/api/appdetails/?appids={}".format(ID)
-	#If the else runs then automatic currency is false 
-	else:
-		#Creates a temporary url for the game, by inserting the appid into the url below
-		tempUrl = "http://store.steampowered.com/api/appdetails/?appids={}&cc={}".format(ID,desiredcurrency)
-	
-	#Calls the PrintInfo function on the tempUrl, appid and the local game number (1,2,3 etc.)
-	PrintInfo(tempUrl,ID,number)
-	#Add's one to number e.g 1 becomes 2, 3 become 4 etc.
-	number += 1
-
-os.system("pause")
