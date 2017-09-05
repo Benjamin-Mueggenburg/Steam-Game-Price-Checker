a7341a9fd176760ac86a4ee73bf9454d6c34a0d4
tintinmovie <31559358+tintinmovie@users.noreply.github.com>
2017-09-06 11:21:46 +1200

    Update SteamGamePriceCheckerModule.py
    Partial change of orginal file to module

--- Function/SteamGamePriceCheckerModule.py
+++ Function/SteamGamePriceCheckerModule.py
@@ -7,12 +7,7 @@ import requests
 
 #Import os, allows for program to pause at end
 import os
-#Version number variable (later to be printed)
-version = "3.0"
-#Author name variable (also later to be printed)
-author = "Benjamin Mueggenburg"
-#Number which with each game will increase by one, saying "Game 1", "Game 2", etc.
-number = 1
+
 
 #System automatically ajusts currency to location. If you want to manually type it in (if you're using TOR or a VPN) then set
 #AutomaticCurreny to 'false'
@@ -22,37 +17,39 @@ automaticCurrency = True
 desiredcurrency = "nzd"
 
 
-#Prints the starting dialogue with the name of the program, the author, version. (\n means new line)
-print("STEAM GAME CHECKER\nversion: {}\nAuthor: {}\n================================\n================================".format(version, author))
+class SteamGameChecker:
+	def __init__(self,desiredcurrency,automaticCurrency=True):
+		self.desiredcurrency = desiredcurrency
+		self.automaticCurrency = autmaticCurrency
 
 
 #The GetInformation function, once inputed with the url for the steam game in the JSON api format and the app's id (Next line)
 #The function will return a list of the game's name, intial price, final price, discount percent and the currency
-def GetInformation(url,appid):
-	#Accesses the WEBapi through the url
-	respose = requests.get(url)
-	#Opens the WEBapi in a json form
-	j = respose.json()
+	def GetInformation(url,appid):
+		#Accesses the WEBapi through the url
+		respose = requests.get(url)
+		#Opens the WEBapi in a json form
+		j = respose.json()
 
-	#Gamename is the variable storing the name of the game
-	Gamename = j["{}".format(appid)]["data"]["name"]
+		#Gamename is the variable storing the name of the game
+		Gamename = j["{}".format(appid)]["data"]["name"]
 
-	#Pre_intial_price is the variable storing the intial price of the product, but the json file doesn't put in decimal places for price (e.g $14.99 become 1499)
-	pre_intial_price = j["{}".format(appid)]["data"]["price_overview"]["initial"]
-	#intial_price is the pre_intial_price but converted into a float and divided by 100, this variable is used instead of pre_intial_price
-	intial_price = float(pre_intial_price) / 100
+		#Pre_intial_price is the variable storing the intial price of the product, but the json file doesn't put in decimal places for price (e.g $14.99 become 1499)
+		pre_intial_price = j["{}".format(appid)]["data"]["price_overview"]["initial"]
+		#intial_price is the pre_intial_price but converted into a float and divided by 100, this variable is used instead of pre_intial_price
+		intial_price = float(pre_intial_price) / 100
 
-	#Pre_final_price is the variable storing the final price of the product, but the json file doesn't put in decimal places for price (e.g $14.99 become 1499)
-	pre_final_price = j["{}".format(appid)]["data"]["price_overview"]["final"]
-	#final_price is the pre_final_price but converted into a float and divided by 100, this variable is used instead of pre_final_price
-	final_price = float(pre_final_price) / 100
+		#Pre_final_price is the variable storing the final price of the product, but the json file doesn't put in decimal places for price (e.g $14.99 become 1499)
+		pre_final_price = j["{}".format(appid)]["data"]["price_overview"]["final"]
+		#final_price is the pre_final_price but converted into a float and divided by 100, this variable is used instead of pre_final_price
+		final_price = float(pre_final_price) / 100
 
-	#discountpercent is the variable storing the discount percent of the game e.g 10, or 75 (it doesn't include the % symbol)
-	discountpercent = j["{}".format(appid)]["data"]["price_overview"]["discount_percent"]
-	#currecy is the variable storing the currency of the prices of the game e.g NZD
-	currency = j["{}".format(appid)]["data"]["price_overview"]["currency"]
-	#returns all the variables in the form of a list
-	return [Gamename, intial_price, final_price, discountpercent, currency]
+		#discountpercent is the variable storing the discount percent of the game e.g 10, or 75 (it doesn't include the % symbol)
+		discountpercent = j["{}".format(appid)]["data"]["price_overview"]["discount_percent"]
+		#currecy is the variable storing the currency of the prices of the game e.g NZD
+		currency = j["{}".format(appid)]["data"]["price_overview"]["currency"]
+		#returns all the variables in the form of a list
+		return [Gamename, intial_price, final_price, discountpercent, currency]
 
 #The PrintInfo function, once inputed with the game's url, appid and number(this was the variable at the top that equals 1) (Next line)
 #It will print out all the details about the game in a more unifrom way
